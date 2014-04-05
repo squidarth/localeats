7fd366a19e9f79f76c3310832dbddcb27712c015
Sid Shanker <sshanker220@gmail.com>
2014-04-05 21:52:53 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Localeats::Application.config.secret_key_base = '26b3b709dc652a980585a6998aa458000e00cc5d32460310e112e271cd89950c9a7cdb75da642ee99a9aa3fa5cbd5d3d46d9056d96450f82f107f8c68db87389'
